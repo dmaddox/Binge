5545085391f70a2d9bcff5eca284852fb1919fa6
Michael Balagia <mbalagia@gmail.com>
2018-01-03 13:19:57 -0600

    filled in schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+
+DROP TABLE IF EXISTS pairings_db;
+
+USE pairings_db;
+
+CREATE TABLE pairs
+(
+	pair_id int NOT NULL AUTO_INCREMENT,
+	media_type varchar(255) NOT NULL,
+	media_title varchar(255) NOT NULL,
+	food_name varchar(255),
+	recipe_url varchar(255),
+	playlist_url varchar(255),
+	drink_name varchar(255),
+	drink_url varchar(255),
+	user_id int,
+	PRIMARY KEY (pair_id)
+);
