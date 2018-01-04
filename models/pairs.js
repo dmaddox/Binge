ae5bdfd5a6a486ada72dee3a361c39c2a33dcda2
dmaddox <dmaddox@gmail.com>
2018-01-04 10:55:53 -0600

    added pairs.js model

---
+++ models/pairs.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+  var Pairs = sequelize.define("Pairs", {
+    pair_id: { 
+      type: DataTypes.INTEGER, 
+      autoIncrement: true,
+      primaryKey: true
+    },
+    media_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    media_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    food_name: DataTypes.STRING,
+    recipe_url: DataTypes.STRING,
+    playlist_url: DataTypes.STRING,
+    drink_name: DataTypes.STRING,
+    drink_url: DataTypes.STRING,
+    user_id: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  })
+
+  return Pairs;
+
+};
